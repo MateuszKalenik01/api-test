5c45f68aa9f69c9fede7d2bef04b6fe0c403c4b0
MateuszKalenik01 <mkalenik@solvd.com>
2024-09-18 14:11:36 +0200

    Added Loggers

--- src/test/java/com/solvd/BaseTest.java
+++ src/test/java/com/solvd/BaseTest.java
@@ -6,6 +6,7 @@ import com.solvd.utils.ConfigReader;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import lombok.extern.slf4j.Slf4j;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -16,16 +17,21 @@ import java.util.Map;
 import static io.restassured.RestAssured.given;
 import static org.testng.Assert.*;
 
+@Slf4j
 public class BaseTest {
 
     @BeforeClass
     public void setup() {
         RestAssured.baseURI = ConfigReader.getProperty("base.url");
+        log.info("Base URL set to: {}", RestAssured.baseURI);
     }
 
     @Test
     public void createUserTest() {
+        log.info("Starting createUserTest");
+
         User newUser = UserService.createUser();
+        log.debug("Created user: {}", newUser);
 
         Response response = given()
                 .auth().oauth2(ConfigReader.getProperty("token"))
@@ -38,20 +44,25 @@ public class BaseTest {
                 .extract()
                 .response();
 
-        response.prettyPrint();
+        log.info("Create user response: {}", response.asString());
 
         String userId = response.jsonPath().getString("id");
         assertNotNull(userId, "User ID should be present in the response");
+        log.info("Created user ID: {}", userId);
 
         assertEquals(response.jsonPath().getString("name"), newUser.getName(), "User name should match");
         assertEquals(response.jsonPath().getString("email"), newUser.getEmail(), "User email should match");
         assertEquals(response.jsonPath().getString("gender"), newUser.getGender().getValue(), "User gender should match");
         assertEquals(response.jsonPath().getString("status"), newUser.getStatus().getValue(), "User status should match");
+        log.info("createUserTest completed successfully");
     }
 
     @Test
     public void TC002_getUserDetailsTest() {
+        log.info("Starting TC002_getUserDetailsTest");
+
         User newUser = UserService.createUser();
+        log.debug("Created user: {}", newUser);
 
         Response createUserResponse = given()
                 .auth().oauth2(ConfigReader.getProperty("token"))
@@ -64,7 +75,10 @@ public class BaseTest {
                 .extract()
                 .response();
 
+        log.info("Create user response: {}", createUserResponse.asString());
+
         String userId = createUserResponse.jsonPath().getString("id");
+        log.info("Created user ID: {}", userId);
 
         Response getUserResponse = given()
                 .auth().oauth2(ConfigReader.getProperty("token"))
@@ -76,18 +90,22 @@ public class BaseTest {
                 .extract()
                 .response();
 
-        getUserResponse.prettyPrint();
+        log.info("Get user response: {}", getUserResponse.asString());
 
         assertEquals(getUserResponse.jsonPath().getString("id"), userId, "User ID should match");
         assertEquals(getUserResponse.jsonPath().getString("name"), newUser.getName(), "User name should match");
         assertEquals(getUserResponse.jsonPath().getString("email"), newUser.getEmail(), "User email should match");
         assertEquals(getUserResponse.jsonPath().getString("gender"), newUser.getGender().getValue(), "User gender should match");
         assertEquals(getUserResponse.jsonPath().getString("status"), newUser.getStatus().getValue(), "User status should match");
+        log.info("TC002_getUserDetailsTest completed successfully");
     }
 
     @Test
     public void TC003_updateUserDetailsTest() {
+        log.info("Starting TC003_updateUserDetailsTest");
+
         User newUser = UserService.createUser();
+        log.debug("Created user: {}", newUser);
 
         Response createUserResponse = given()
                 .auth().oauth2(ConfigReader.getProperty("token"))
@@ -100,7 +118,10 @@ public class BaseTest {
                 .extract()
                 .response();
 
+        log.info("Create user response: {}", createUserResponse.asString());
+
         String userId = createUserResponse.jsonPath().getString("id");
+        log.info("Created user ID: {}", userId);
 
         User updatedUser = User.builder()
                 .name("Updated Namez")
@@ -108,6 +129,8 @@ public class BaseTest {
                 .gender(newUser.getGender())
                 .status(newUser.getStatus())
                 .build();
+
+        log.debug("Updated user details: {}", updatedUser);
 
         Response updateUserResponse = given()
                 .auth().oauth2(ConfigReader.getProperty("token"))
@@ -120,17 +143,21 @@ public class BaseTest {
                 .extract()
                 .response();
 
-        updateUserResponse.prettyPrint();
+        log.info("Update user response: {}", updateUserResponse.asString());
 
         assertEquals(updateUserResponse.jsonPath().getString("name"), updatedUser.getName(), "Updated name should match");
         assertEquals(updateUserResponse.jsonPath().getString("email"), updatedUser.getEmail(), "Updated email should match");
         assertEquals(updateUserResponse.jsonPath().getString("gender"), updatedUser.getGender().getValue(), "User gender should match");
         assertEquals(updateUserResponse.jsonPath().getString("status"), updatedUser.getStatus().getValue(), "User status should match");
+        log.info("TC003_updateUserDetailsTest completed successfully");
     }
 
     @Test
     public void TC004_patchUserDetailsTest() {
+        log.info("Starting TC004_patchUserDetailsTest");
+
         User newUser = UserService.createUser();
+        log.debug("Created user: {}", newUser);
 
         Response createUserResponse = given()
                 .auth().oauth2(ConfigReader.getProperty("token"))
@@ -143,10 +170,15 @@ public class BaseTest {
                 .extract()
                 .response();
 
+        log.info("Create user response: {}", createUserResponse.asString());
+
         String userId = createUserResponse.jsonPath().getString("id");
+        log.info("Created user ID: {}", userId);
 
         Map<String, String> updatedData = new HashMap<>();
         updatedData.put("name", "Patched Name");
+
+        log.debug("Patch data: {}", updatedData);
 
         Response patchUserResponse = given()
                 .auth().oauth2(ConfigReader.getProperty("token"))
@@ -159,17 +191,22 @@ public class BaseTest {
                 .extract()
                 .response();
 
-        patchUserResponse.prettyPrint();
+        log.info("Patch user response: {}", patchUserResponse.asString());
 
         assertEquals(patchUserResponse.jsonPath().getString("name"), updatedData.get("name"), "User name should be updated");
         assertEquals(patchUserResponse.jsonPath().getString("email"), newUser.getEmail(), "User email should remain unchanged");
         assertEquals(patchUserResponse.jsonPath().getString("gender"), newUser.getGender().getValue(), "User gender should remain unchanged");
         assertEquals(patchUserResponse.jsonPath().getString("status"), newUser.getStatus().getValue(), "User status should remain unchanged");
+
+        log.info("TC004_patchUserDetailsTest completed successfully");
     }
 
     @Test
     public void TC005_deleteUserTest() {
+        log.info("Starting TC005_deleteUserTest");
+
         User newUser = UserService.createUser();
+        log.debug("Created user: {}", newUser);
 
         Response createUserResponse = given()
                 .auth().oauth2(ConfigReader.getProperty("token"))
@@ -182,7 +219,10 @@ public class BaseTest {
                 .extract()
                 .response();
 
+        log.info("Create user response: {}", createUserResponse.asString());
+
         String userId = createUserResponse.jsonPath().getString("id");
+        log.info("Created user ID: {}", userId);
 
         given()
                 .auth().oauth2(ConfigReader.getProperty("token"))
@@ -191,17 +231,25 @@ public class BaseTest {
                 .then()
                 .statusCode(204);
 
+        log.info("User {} deleted successfully", userId);
+
         given()
                 .auth().oauth2(ConfigReader.getProperty("token"))
                 .when()
                 .get("/users/" + userId)
                 .then()
                 .statusCode(404);
+
+        log.info("Verified user {} is deleted", userId);
+        log.info("TC005_deleteUserTest completed successfully");
     }
 
     @Test
     public void TC006_createPostForUserTest() {
+        log.info("Starting TC006_createPostForUserTest");
+
         User newUser = UserService.createUser();
+        log.debug("Created user: {}", newUser);
 
         Response createUserResponse = given()
                 .auth().oauth2(ConfigReader.getProperty("token"))
@@ -214,11 +262,15 @@ public class BaseTest {
                 .extract()
                 .response();
 
+        log.info("Create user response: {}", createUserResponse.asString());
+
         String userId = createUserResponse.jsonPath().getString("id");
+        log.info("Created user ID: {}", userId);
 
         Map<String, String> postData = new HashMap<>();
         postData.put("title", "My First Post");
         postData.put("body", "This is the content of the first post.");
+        log.debug("Post data: {}", postData);
 
         Response createPostResponse = given()
                 .auth().oauth2(ConfigReader.getProperty("token"))
@@ -231,17 +283,22 @@ public class BaseTest {
                 .extract()
                 .response();
 
-        createPostResponse.prettyPrint();
+        log.info("Create post response: {}", createPostResponse.asString());
 
         assertNotNull(createPostResponse.jsonPath().getString("id"), "Post ID should be present in the response");
         assertEquals(createPostResponse.jsonPath().getString("title"), postData.get("title"), "Post title should match");
         assertEquals(createPostResponse.jsonPath().getString("body"), postData.get("body"), "Post body should match");
         assertEquals(createPostResponse.jsonPath().getString("user_id"), userId, "User ID should match");
+
+        log.info("TC006_createPostForUserTest completed successfully");
     }
 
     @Test
     public void TC007_getPostsByUserTest() {
+        log.info("Starting TC007_getPostsByUserTest");
+
         User newUser = UserService.createUser();
+        log.debug("Created user: {}", newUser);
 
         Response createUserResponse = given()
                 .auth().oauth2(ConfigReader.getProperty("token"))
@@ -254,11 +311,15 @@ public class BaseTest {
                 .extract()
                 .response();
 
+        log.info("Create user response: {}", createUserResponse.asString());
+
         String userId = createUserResponse.jsonPath().getString("id");
+        log.info("Created user ID: {}", userId);
 
         Map<String, String> postData1 = new HashMap<>();
         postData1.put("title", "First Post");
         postData1.put("body", "This is the content of the first post.");
+        log.debug("First post data: {}", postData1);
 
         given()
                 .auth().oauth2(ConfigReader.getProperty("token"))
@@ -272,6 +333,7 @@ public class BaseTest {
         Map<String, String> postData2 = new HashMap<>();
         postData2.put("title", "Second Post");
         postData2.put("body", "This is the content of the second post.");
+        log.debug("Second post data: {}", postData2);
 
         given()
                 .auth().oauth2(ConfigReader.getProperty("token"))
@@ -292,10 +354,11 @@ public class BaseTest {
                 .extract()
                 .response();
 
-        getPostsResponse.prettyPrint();
+        log.info("Get posts response: {}", getPostsResponse.asString());
 
         List<Map<String, String>> posts = getPostsResponse.jsonPath().getList("$");
         assertNotNull(posts, "Posts should be returned for the user");
+        assertTrue(posts.size() >= 2, "There should be at least 2 posts returned");
 
         boolean foundFirstPost = false;
         boolean foundSecondPost = false;
@@ -312,11 +375,15 @@ public class BaseTest {
 
         assertTrue(foundFirstPost, "First post should be found");
         assertTrue(foundSecondPost, "Second post should be found");
+        log.info("TC007_getPostsByUserTest completed successfully");
     }
 
     @Test
     public void TC008_createCommentForPostTest() {
+        log.info("Starting TC008_createCommentForPostTest");
+
         User newUser = UserService.createUser();
+        log.debug("Created user: {}", newUser);
 
         Response createUserResponse = given()
                 .auth().oauth2(ConfigReader.getProperty("token"))
@@ -329,11 +396,15 @@ public class BaseTest {
                 .extract()
                 .response();
 
+        log.info("Create user response: {}", createUserResponse.asString());
+
         String userId = createUserResponse.jsonPath().getString("id");
+        log.info("Created user ID: {}", userId);
 
         Map<String, String> postData = new HashMap<>();
         postData.put("title", "Post for Comment");
         postData.put("body", "This post is for testing comment creation.");
+        log.debug("Post data: {}", postData);
 
         Response createPostResponse = given()
                 .auth().oauth2(ConfigReader.getProperty("token"))
@@ -346,12 +417,16 @@ public class BaseTest {
                 .extract()
                 .response();
 
+        log.info("Create post response: {}", createPostResponse.asString());
+
         String postId = createPostResponse.jsonPath().getString("id");
+        log.info("Created post ID: {}", postId);
 
         Map<String, String> commentData = new HashMap<>();
         commentData.put("name", "Test Commenter");
         commentData.put("email", "test_commenter@example.com");
         commentData.put("body", "This is a test comment.");
+        log.debug("Comment data: {}", commentData);
 
         Response createCommentResponse = given()
                 .auth().oauth2(ConfigReader.getProperty("token"))
@@ -364,17 +439,22 @@ public class BaseTest {
                 .extract()
                 .response();
 
-        createCommentResponse.prettyPrint();
+        log.info("Create comment response: {}", createCommentResponse.asString());
 
         assertNotNull(createCommentResponse.jsonPath().getString("id"), "Comment ID should be present in the response");
         assertEquals(createCommentResponse.jsonPath().getString("name"), commentData.get("name"), "Comment name should match");
         assertEquals(createCommentResponse.jsonPath().getString("email"), commentData.get("email"), "Comment email should match");
         assertEquals(createCommentResponse.jsonPath().getString("body"), commentData.get("body"), "Comment body should match");
+
+        log.info("TC008_createCommentForPostTest completed successfully");
     }
 
     @Test
     public void TC009_getCommentsForPostTest() {
+        log.info("Starting TC009_getCommentsForPostTest");
+
         User newUser = UserService.createUser();
+        log.debug("Created user: {}", newUser);
 
         Response createUserResponse = given()
                 .auth().oauth2(ConfigReader.getProperty("token"))
@@ -387,11 +467,15 @@ public class BaseTest {
                 .extract()
                 .response();
 
+        log.info("Create user response: {}", createUserResponse.asString());
+
         String userId = createUserResponse.jsonPath().getString("id");
+        log.info("Created user ID: {}", userId);
 
         Map<String, String> postData = new HashMap<>();
         postData.put("title", "Post for Comment Retrieval");
         postData.put("body", "This post is for testing comment retrieval.");
+        log.debug("Post data: {}", postData);
 
         Response createPostResponse = given()
                 .auth().oauth2(ConfigReader.getProperty("token"))
@@ -404,12 +488,16 @@ public class BaseTest {
                 .extract()
                 .response();
 
+        log.info("Create post response: {}", createPostResponse.asString());
+
         String postId = createPostResponse.jsonPath().getString("id");
+        log.info("Created post ID: {}", postId);
 
         Map<String, String> commentData1 = new HashMap<>();
         commentData1.put("name", "First Commenter");
         commentData1.put("email", "first_commenter@example.com");
         commentData1.put("body", "This is the first comment.");
+        log.debug("First comment data: {}", commentData1);
 
         given()
                 .auth().oauth2(ConfigReader.getProperty("token"))
@@ -424,6 +512,7 @@ public class BaseTest {
         commentData2.put("name", "Second Commenter");
         commentData2.put("email", "second_commenter@example.com");
         commentData2.put("body", "This is the second comment.");
+        log.debug("Second comment data: {}", commentData2);
 
         given()
                 .auth().oauth2(ConfigReader.getProperty("token"))
@@ -444,7 +533,7 @@ public class BaseTest {
                 .extract()
                 .response();
 
-        getCommentsResponse.prettyPrint();
+        log.info("Get comments response: {}", getCommentsResponse.asString());
 
         List<Map<String, String>> comments = getCommentsResponse.jsonPath().getList("$");
         assertNotNull(comments, "Comments should be returned for the post");
@@ -465,11 +554,16 @@ public class BaseTest {
 
         assertTrue(foundFirstComment, "First comment should be found");
         assertTrue(foundSecondComment, "Second comment should be found");
+
+        log.info("TC009_getCommentsForPostTest completed successfully");
     }
 
     @Test
     public void TC010_createTodoForUserTest() {
+        log.info("Starting TC010_createTodoForUserTest");
+
         User newUser = UserService.createUser();
+        log.debug("Created user: {}", newUser);
 
         Response createUserResponse = given()
                 .auth().oauth2(ConfigReader.getProperty("token"))
@@ -482,12 +576,16 @@ public class BaseTest {
                 .extract()
                 .response();
 
+        log.info("Create user response: {}", createUserResponse.asString());
+
         String userId = createUserResponse.jsonPath().getString("id");
+        log.info("Created user ID: {}", userId);
 
         Map<String, String> todoData = new HashMap<>();
         todoData.put("title", "Complete the report");
         todoData.put("due_on", "2024-09-30T00:00:00.000+05:30");
         todoData.put("status", "pending");
+        log.debug("Todo data: {}", todoData);
 
         Response createTodoResponse = given()
                 .auth().oauth2(ConfigReader.getProperty("token"))
@@ -500,11 +598,13 @@ public class BaseTest {
                 .extract()
                 .response();
 
-        createTodoResponse.prettyPrint();
+        log.info("Create todo response: {}", createTodoResponse.asString());
 
         assertNotNull(createTodoResponse.jsonPath().getString("id"), "Todo ID should be present in the response");
         assertEquals(createTodoResponse.jsonPath().getString("title"), todoData.get("title"), "Todo title should match");
         assertEquals(createTodoResponse.jsonPath().getString("due_on"), todoData.get("due_on"), "Todo due date should match");
         assertEquals(createTodoResponse.jsonPath().getString("status"), todoData.get("status"), "Todo status should match");
+
+        log.info("TC010_createTodoForUserTest completed successfully");
     }
 }
--- src/test/java/com/solvd/GraphQl.java
+++ src/test/java/com/solvd/GraphQl.java
@@ -6,6 +6,7 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 import java.util.Map;
@@ -13,15 +14,21 @@ import java.util.Map;
 import static io.restassured.RestAssured.given;
 import static org.testng.Assert.*;
 
+@Slf4j
 public class GraphQl {
+
     @BeforeClass
     public void setup() {
         RestAssured.baseURI = ConfigReader.getProperty("base.url");
+        log.info("Base URL set to: {}", RestAssured.baseURI);
     }
 
     @Test
     public void TC001_queryAllUsersWithoutTokenTest() {
+        log.info("Starting TC001_queryAllUsersWithoutTokenTest");
+
         String query = ConfigReader.getProperty("users");
+        log.debug("GraphQL query: {}", query);
 
         Response response = given()
                 .contentType(ContentType.JSON)
@@ -33,6 +40,7 @@ public class GraphQl {
                 .extract()
                 .response();
 
+        log.info("Response received: {}", response.asString());
         response.prettyPrint();
 
         List<Map<String, String>> users = response.jsonPath().getList("data.users.nodes");
@@ -46,11 +54,17 @@ public class GraphQl {
             assertNotNull(user.get("email"), "The user's email should be present");
             assertNotNull(user.get("status"), "User status should be present");
         }
+
+        log.info("TC001_queryAllUsersWithoutTokenTest completed successfully");
     }
+
     @Test
     public void TC002_fetchUserByIdTest() {
+        log.info("Starting TC002_fetchUserByIdTest");
+
         String userId = "7409749";
         String query = ConfigReader.getProperty("user.id").replace("{id}", userId);
+        log.debug("GraphQL query for fetching user by ID: {}", query);
 
         Response response = given()
                 .contentType(ContentType.JSON)
@@ -62,6 +76,7 @@ public class GraphQl {
                 .extract()
                 .response();
 
+        log.info("Response received: {}", response.asString());
         response.prettyPrint();
 
         String returnedId = response.jsonPath().getString("data.user.id");
@@ -71,12 +86,16 @@ public class GraphQl {
         assertNotNull(response.jsonPath().getString("data.user.email"), "The user's email should not be empty");
         assertNotNull(response.jsonPath().getString("data.user.gender"), "User gender should not be blank");
         assertNotNull(response.jsonPath().getString("data.user.status"), "User status should not be blank");
+
+        log.info("TC002_fetchUserByIdTest completed successfully");
     }
 
     @Test
     public void TC003_createUserTest() {
+        log.info("Starting TC003_createUserTest");
+
         String name = "Test User";
-        String email = "tedsasdsadsvdsvst@example.com";
+        String email = "testuser@example.com";
         String gender = "male";
         String status = "active";
 
@@ -85,6 +104,7 @@ public class GraphQl {
                 .replace("{email}", email)
                 .replace("{gender}", gender)
                 .replace("{status}", status);
+        log.debug("GraphQL query for creating user: {}", query);
 
         Response response = given()
                 .header("Authorization", "Bearer " + ConfigReader.getProperty("token"))
@@ -97,22 +117,28 @@ public class GraphQl {
                 .extract()
                 .response();
 
+        log.info("Response received: {}", response.asString());
         response.prettyPrint();
 
         assertEquals(response.jsonPath().getString("data.createUser.user.name"), name, "The username should match");
         assertEquals(response.jsonPath().getString("data.createUser.user.email"), email, "The user's email should match");
         assertEquals(response.jsonPath().getString("data.createUser.user.gender"), gender, "The user's gender should match");
         assertEquals(response.jsonPath().getString("data.createUser.user.status"), status, "The user status should match");
+
+        log.info("TC003_createUserTest completed successfully");
     }
 
     @Test
     public void TC004_updateUserTest() {
+        log.info("Starting TC004_updateUserTest");
+
         String userId = "7409788";
         String newName = "Updated Name";
 
         String query = ConfigReader.getProperty("update.user")
                 .replace("{id}", userId)
                 .replace("{name}", newName);
+        log.debug("GraphQL query for updating user: {}", query);
 
         Response response = given()
                 .header("Authorization", "Bearer " + ConfigReader.getProperty("token"))
@@ -125,16 +151,20 @@ public class GraphQl {
                 .extract()
                 .response();
 
+        log.info("Response received: {}", response.asString());
         response.prettyPrint();
-
 
         String updatedId = response.jsonPath().getString("data.updateUser.user.id");
         assertEquals(updatedId, userId, "The user ID should match");
 
         assertEquals(response.jsonPath().getString("data.updateUser.user.name"), newName, "The username should be updated");
+
+        log.info("TC004_updateUserTest completed successfully");
     }
+
     @Test
     public void TC005_createPostForUserTest() {
+        log.info("Starting TC005_createPostForUserTest");
 
         String userId = "7412502";
         String title = "My Test Post";
@@ -144,6 +174,7 @@ public class GraphQl {
                 .replace("{id}", userId)
                 .replace("{title}", title)
                 .replace("{body}", body);
+        log.debug("GraphQL query for creating post: {}", query);
 
         Response response = given()
                 .header("Authorization", "Bearer " + ConfigReader.getProperty("token"))
@@ -156,13 +187,15 @@ public class GraphQl {
                 .extract()
                 .response();
 
+        log.info("Response received: {}", response.asString());
         response.prettyPrint();
 
         assertNotNull(response.jsonPath().getString("data.createPost.post"), "The reply should contain the data of the post you created");
-
         assertEquals(response.jsonPath().getString("data.createPost.post.userId"), userId, "The user ID should match");
         assertEquals(response.jsonPath().getString("data.createPost.post.title"), title, "The post title should fit");
         assertEquals(response.jsonPath().getString("data.createPost.post.body"), body, "The content of the post should match");
-    }
 
+        log.info("TC005_createPostForUserTest completed successfully");
+    }
 }
+
